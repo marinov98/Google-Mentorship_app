0d1033f78ce8faedf0ddd855c04656edbd9c594e
Marin Marinov <Marin.Marinov38@myhunter.cuny.edu>
2019-02-19 00:49:24 -0500

    used some angular!

--- environmentalism-app/src/app/app.module.ts
+++ environmentalism-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecyclingComponent } from './recycling/recycling.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecyclingComponent
   ],
   imports: [
     BrowserModule,
---
+++ environmentalism-app/src/app/recycling/recycling.component.ts
@@ -0,0 +1,44 @@
+import { Component} from '@angular/core';
+
+@Component({
+    selector: 'recycling',
+    template: `
+    <h2 class="goodTitle">{{ goodMaterials }}</h2> 
+      <div class=row>
+        <div class="col-sm-6">
+          <ul>
+            <li *ngFor="let materials of goodMaterialList">
+                {{materials}}
+            </li>
+          </ul>
+        </div>
+        <div class="col-sm-6">
+            <h2>Description</h2>
+            <p>Something Something</p>
+        </div>
+      </div>
+    <h2 class="badTitle">{{ badMaterials }}</h2>
+    <div class="row"> 
+      <div class="col-sm-6">
+          <ul>
+            <li *ngFor="let materials of badMaterialList">
+                {{materials}}
+            </li>
+          </ul>
+        </div>
+        <div class="col-sm-6">
+            <h2>Description</h2>
+            <p>Something Something</p>
+        </div>
+    </div>`
+
+              
+})
+
+export class RecyclingComponent { 
+
+  goodMaterials = "List of good recycling materials";
+  badMaterials = "List of bad recycling mateirals";
+  goodMaterialList = ["Aluminum","Cardboard","Paper"];
+  badMaterialList = ["Glass","Plastic"];
+}
